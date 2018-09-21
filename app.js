22c25ea8143fd823afaecfce3033b239efa2b678
bengben <bengben@hotmail.com>
2018-09-21 18:10:06 +0800

    完成,新增文章信息

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var newsRouter = require('./routes/news');
 
 var app = express();
 
@@ -19,10 +18,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(bodyParser.json()); // for parsing application/json
+// app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/news', newsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,53 @@
 var express = require('express');
-var router = express.Router();
+var db 			= require('../model/db.js');
+
+var router 	= express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/api', function(req, res, next) {
-  res.send({ title: 'Express' });
+/* news */
+router.get('/news/:p', function(req, res, next) {
+	let p  				= req.params.p;
+	let numStart 	= p * 10;
+	if(p=='1'){
+		numStart = 0;
+	}
+	let numEnd 		= numStart+10;
+	let nextPage	=	parseInt(p)+1;
+	let page 			= {p:p,numStart:numStart,numEnd:numEnd,nextPage:nextPage};// 
+	let sql 			=	"select "
+  res.json({p:p,numStart:numStart,numEnd:numEnd,nextPage:nextPage})
 });
 
-router.get('/v1/user/:id', function(req, res, next) {
-  res.send({ 'User Id': req.params.id });
+
+router.post('/insert', function(req, res, next) {
+	console.log(req.body);
+	let sd = require('silly-datetime');
+	let time=sd.format(new Date(), 'YYYY-MM-DD HH:mm:ss');
+	let news = {tag: req.body.tag, author: req.body.author,avatar:req.body.avatar,createTime:time,content:req.body.content,photo:req.body.photo};
+  let sqlString = 'INSERT INTO article SET ?';
+  let connection = db.connection();
+  db.insert(connection, sqlString, news, function(id){
+      console.log('inserted id is:' + id);
+  });
+  db.close(connection);
+  res.json(news);
+});
+
+
+//插入测试
+router.get('/dbtest',function(req, res){
+    let project = {project_name: 'test', create_time: '2017-03-28 14:09:29'};
+    let sqlString = 'INSERT INTO project SET ?';
+    let connection = db.connection();
+    db.insert(connection, sqlString, project, function(id){
+        console.log('inserted id is:' + id);
+    });
+    db.close(connection);
+    return;
 });
 
 module.exports = router;
